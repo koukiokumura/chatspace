3f62bfaaf7651817088014e3e251d359d837871a
奥村光貴 <>
2019-02-14 14:54:56 +0900

    Initial commit

--- chat-space/config/unicorn.rb
+++ chat-space/config/unicorn.rb
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 1
@@ -6,6 +7,16 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
+=======
+worker_processes 1
+
+app_path = File.expand_path('../../../', __FILE__)
+working_directory "#{app_path}/current"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+>>>>>>> Stashed changes
 
 listen 3000
 timeout 60
